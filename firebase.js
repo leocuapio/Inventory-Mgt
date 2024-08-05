5012b2bc4133c53ca2ae7ad0abe63a3bd99f8e3f
leocuapio <cuapioleonardo7@gmail.com>
2024-08-04 20:24:34 -0400

    finished

--- firebase.js
+++ firebase.js
@@ -1,12 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from 'firebase/auth'; // Import the function correctly
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBHALzlgqrYAuH5AwH_rHslqwZvyCtXruU",
   authDomain: "inventory-management-app-a3784.firebaseapp.com",
@@ -19,7 +16,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 const firestore = getFirestore(app);
+const auth = getAuth(app);
 
-export {firestore}
+export { firestore, auth };
